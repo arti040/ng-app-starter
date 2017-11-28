fafe10f08d06097a4218bab445d927a576d6fedb
ppotera <ja@piotrpotera.com>
2017-11-28 13:12:26 +0100

    Working on tests.

---
+++ app/components/dummy.component.spec.ts
@@ -0,0 +1,37 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By }                        from '@angular/platform-browser';
+import { DebugElement }              from '@angular/core';
+import { DummyComponent }            from './dummy.component';
+
+describe('Dummy component', () => {
+
+  let comp:    DummyComponent;
+  let fixture: ComponentFixture<DummyComponent>;
+  let de:      DebugElement;
+  let el:      HTMLElement;
+
+  beforeEach(()=>{
+    TestBed.configureTestingModule({ declarations: [ DummyComponent ] })
+  });
+ 
+  fixture = TestBed.createComponent(DummyComponent)
+  comp = fixture.componentInstance;
+  de = fixture.debugElement.query(By.css('h1'));
+  el = de.nativeElement;
+
+  it('No title in the DOM until manually call `detectChanges`', () => {
+    expect(el.textContent).toEqual('');
+  });
+
+  it('Should display default title', () => {
+    fixture.detectChanges();
+    expect(el.textContent).toContain(comp.mainTitle);
+  });
+  
+  it('Should display binded title', () => {
+    comp.title = 'Binded Title'
+    fixture.detectChanges();
+    expect(el.textContent).toContain('Binded Title');
+  });
+
+});
--- app/components/dummy.component.ts
+++ app/components/dummy.component.ts
@@ -2,12 +2,14 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: '<dummy-component></dummy-component>',
-  template: '<h1>{{ title }}</h1>',
+  template: '<h1>{{ mainTitle }}</h1>',
 })
 export class DummyComponent {
 
 	@Input() title: string;
-
+	
+	public mainTitle: string = this.title || 'Dummy Title';
+	
 	constructor() {
 		console.log('DummyComponent\'s saying Yo!');
 	}
